ac1c68ea95454a4c70892c393b12bb8a744e1125
Gary Burgess <gary.burgess@gmail.com>
2016-03-31 17:51:56 +0100

    Add timeout for spawning

--- test/src/Util/Process.purs
+++ test/src/Util/Process.purs
@@ -14,17 +14,14 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -}
 
-module Test.Util.Process where
+module Test.Util.Process (spawnMongo, spawnQuasar) where
 
 import Prelude
 
-import Control.Apply ((*>))
-import Control.Bind ((=<<))
-import Control.Monad.Aff (Aff, launchAff)
-import Control.Monad.Aff.AVar (AVAR, makeVar, takeVar, putVar)
-import Control.Monad.Eff (Eff)
+import Control.Monad.Aff (Aff, launchAff, later', forkAff)
+import Control.Monad.Aff.AVar (AVAR, makeVar, takeVar, putVar, killVar)
 import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Exception (EXCEPTION)
+import Control.Monad.Eff.Exception (EXCEPTION, error)
 import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Aff.Console (log)
 
@@ -35,7 +32,6 @@ import Node.Buffer (BUFFER)
 import Node.ChildProcess as CP
 import Node.FS (FS)
 import Node.FS.Aff as FSA
-import Node.Process as Proc
 import Node.Stream as Stream
 import Node.Encoding as Enc
 
@@ -45,46 +41,39 @@ spawnMongo ∷ ∀ eff. Aff (avar ∷ AVAR, cp ∷ CP.CHILD_PROCESS, fs ∷ FS, 
 spawnMongo = do
   FS.rmRec "test/tmp/db"
   FS.mkdirRec "test/tmp/db"
-  var ← makeVar
-  log "Starting mongod..."
-  proc ← liftEff do
-    cp ← CP.spawn
+  spawn "MongoDB" "[initandlisten] waiting for connections" $ liftEff $
+    CP.spawn
       "mongod"
-      ["--port", "63174", "--dbpath", "db"]
+      (Str.split " " "--port 63174 --dbpath db")
       (CP.defaultSpawnOptions { cwd = Just "test/tmp" })
-    Stream.onDataString (CP.stdout cp) Enc.UTF8 \s -> launchAff do
-      if isJust (Str.indexOf "[initandlisten] waiting for connections on port" s)
-        then putVar var unit
-        else pure unit
-    pure cp
-  takeVar var *> log "Started"
-  pure proc
 
 spawnQuasar ∷ ∀ eff. Aff (avar ∷ AVAR, cp ∷ CP.CHILD_PROCESS, fs ∷ FS, buffer ∷ BUFFER, console ∷ CONSOLE, err ∷ EXCEPTION | eff) CP.ChildProcess
 spawnQuasar = do
   FS.rmRec "test/tmp/quasar"
   FS.mkdirRec "test/tmp/quasar"
-  buf ← FSA.readFile "test/quasar/config.json"
-  FSA.writeFile "test/tmp/quasar/config.json" buf
-  var ← makeVar
-  log "Starting Quasar..."
-  proc ← liftEff do
-    cp ←
-      CP.spawn
-        "java"
-        ["-jar", "../../quasar/quasar.jar", "-c", "config.json"]
-        (CP.defaultSpawnOptions { cwd = Just "test/tmp/quasar" })
-    Stream.onDataString (CP.stdout cp) Enc.UTF8 \s -> launchAff do
-      if isJust (Str.indexOf "Press Enter to stop" s)
-        then putVar var unit
-        else pure unit
-    pure cp
-  takeVar var *> log "Started"
-  pure proc
+  FSA.readFile "test/quasar/config.json" >>= FSA.writeFile "test/tmp/quasar/config.json"
+  spawn "Quasar" "Press Enter to stop" $ liftEff $
+    CP.spawn
+      "java"
+      (Str.split " " "-jar ../../quasar/quasar.jar -c config.json")
+      (CP.defaultSpawnOptions { cwd = Just "test/tmp/quasar" })
 
--- pipeOutputs ∷ ∀ eff. CP.ChildProcess → Eff (cp ∷ CP.CHILD_PROCESS, console ∷ CONSOLE, err ∷ EXCEPTION | eff) CP.ChildProcess
--- pipeOutputs cp = do
---   Stream.pipe (CP.stdout cp) Proc.stdout
---   Stream.pipe (CP.stderr cp) Proc.stderr
---   CP.onExit cp Debug.Trace.traceAnyA
---   pure cp
+spawn
+  ∷ ∀ eff
+  . String
+  → String
+  → Aff (avar ∷ AVAR, cp ∷ CP.CHILD_PROCESS, console ∷ CONSOLE, err ∷ EXCEPTION | eff) CP.ChildProcess
+  → Aff (avar ∷ AVAR, cp ∷ CP.CHILD_PROCESS, console ∷ CONSOLE, err ∷ EXCEPTION | eff) CP.ChildProcess
+spawn name startLine spawnProc = do
+  log $ "Starting " ++ name ++ "..."
+  var ← makeVar
+  proc ← spawnProc
+  liftEff $ Stream.onDataString (CP.stdout proc) Enc.UTF8 \s ->
+    launchAff
+      if isJust (Str.indexOf startLine s)
+      then putVar var unit
+      else pure unit
+  forkAff $ later' 10000 $ killVar var (error "Timed out")
+  v ← takeVar var
+  log "Started"
+  pure proc
