34a88001d769f5481bb91b0fb8c8d1edcbd45ef7
Nathan Faubion <nathan@n-son.com>
2017-03-31 14:55:59 -0500

    Use untyped properties like Mongo

--- src/Quasar/Mount/SparkHDFS.purs
+++ src/Quasar/Mount/SparkHDFS.purs
@@ -32,7 +32,6 @@ import Data.Either (Either(..))
 import Data.List as L
 import Data.Maybe (Maybe(..), maybe)
 import Data.StrMap as SM
-import Data.Traversable (traverse)
 import Data.Tuple (Tuple(..))
 import Data.URI as URI
 import Data.URI.Path (printPath, parseURIPathAbs)
@@ -49,24 +48,7 @@ type Config =
   { sparkHost ∷ Host
   , hdfsHost ∷ Host
   , path ∷ Maybe AnyPath
-  , executorMemory ∷ Maybe String
-  , executorCores ∷ Maybe Int
-  , executorExtraJavaOptions ∷ Maybe String
-  , defaultParallelism ∷ Maybe Int
-  , filesMaxPartitionBytes ∷ Maybe Number
-  , driverCores ∷ Maybe Int
-  , driverMaxResultSize ∷ Maybe String
-  , driverMemory ∷ Maybe String
-  , localDir ∷ Maybe String
-  , reducerMaxSizeInFlight ∷ Maybe String
-  , reducerMaxReqsInFlight ∷ Maybe Int
-  , shuffleFileBuffer ∷ Maybe String
-  , shuffleIoRetryWait ∷ Maybe String
-  , memoryFraction ∷ Maybe Number
-  , memoryStorageFraction ∷ Maybe Number
-  , coresMax ∷ Maybe Number
-  , speculation ∷ Maybe Boolean
-  , tasksCpus ∷ Maybe Int
+  , props ∷ SM.StrMap (Maybe String)
   }
 
 toJSON ∷ Config → Json
@@ -89,102 +71,43 @@ fromJSONString ∷ ∀ a. J.DecodeJson a ⇒ String → Either String a
 fromJSONString = J.decodeJson <=< J.jsonParser
 
 toURI ∷ Config → URI.AbsoluteURI
-toURI cfg = mkURI sparkURIScheme cfg.sparkHost (Just (URI.Query props))
+toURI cfg = mkURI sparkURIScheme cfg.sparkHost (Just (URI.Query $ requiredProps <> optionalProps))
   where
-  props ∷ L.List (Tuple String (Maybe String))
-  props = L.mapMaybe (map (map Just)) $ L.fromFoldable
-    [ Tuple "hdfsUrl" <$> Just (encodeURIComponent $ URI.printAbsoluteURI $ mkURI hdfsURIScheme cfg.hdfsHost Nothing)
-    , Tuple "rootPath" <$> Just (maybe "/" printPath cfg.path)
-    , Tuple "spark.executor.memory" <$> cfg.executorMemory
-    , Tuple "spark.executor.cores" <<< toJSONString <$> cfg.executorCores
-    , Tuple "spark.executor.extraJavaOptions" <$> cfg.executorExtraJavaOptions
-    , Tuple "spark.default.parallelism" <<< toJSONString <$> cfg.defaultParallelism
-    , Tuple "spark.files.maxPartitionBytes" <<< toJSONString <$> cfg.filesMaxPartitionBytes
-    , Tuple "spark.driver.cores" <<< toJSONString <$> cfg.driverCores
-    , Tuple "spark.driver.maxResultSize" <$> cfg.driverMaxResultSize
-    , Tuple "spark.driver.memory" <$> cfg.driverMemory
-    , Tuple "spark.local.dir" <$> cfg.localDir
-    , Tuple "spark.reducer.maxSizeInFlight" <$> cfg.reducerMaxSizeInFlight
-    , Tuple "spark.reducer.maxReqsInFlight" <<< toJSONString <$> cfg.reducerMaxReqsInFlight
-    , Tuple "spark.shuffle.file.buffer" <$> cfg.shuffleFileBuffer
-    , Tuple "spark.shuffle.io.retryWait" <$> cfg.shuffleIoRetryWait
-    , Tuple "spark.memory.fraction" <<< toJSONString <$> cfg.memoryFraction
-    , Tuple "spark.memory.storageFraction" <<< toJSONString <$> cfg.memoryStorageFraction
-    , Tuple "spark.cores.max" <<< toJSONString <$> cfg.coresMax
-    , Tuple "spark.speculation" <<< toJSONString <$> cfg.speculation
-    , Tuple "spark.tasks.cpus" <<< toJSONString <$> cfg.tasksCpus
+  requiredProps ∷ L.List (Tuple String (Maybe String))
+  requiredProps = L.fromFoldable
+    [ Tuple "hdfsUrl" $ Just $ encodeURIComponent $ URI.printAbsoluteURI $ mkURI hdfsURIScheme cfg.hdfsHost Nothing
+    , Tuple "rootPath" $ Just $ maybe "/" printPath cfg.path
     ]
 
-  mkURI ∷ URI.URIScheme → Host → Maybe URI.Query → URI.AbsoluteURI
-  mkURI scheme host params =
-    URI.AbsoluteURI
-      (Just scheme)
-      (URI.HierarchicalPart (Just (URI.Authority Nothing (pure host))) Nothing)
-      params
+  optionalProps ∷ L.List (Tuple String (Maybe String))
+  optionalProps = SM.toUnfoldable cfg.props
 
 fromURI ∷ URI.AbsoluteURI → Either String Config
 fromURI (URI.AbsoluteURI scheme (URI.HierarchicalPart auth _) query) = do
   unless (scheme == Just sparkURIScheme) $ Left "Expected `spark` URL scheme"
   sparkHost ← extractHost auth
+  let props = maybe SM.empty (\(URI.Query qs) → SM.fromFoldable qs) query
 
-  let
-    props = maybe SM.empty (\(URI.Query qs) → SM.fromFoldable qs) query
-    lookup = join <<< flip SM.lookup props
-    lookupJSON ∷ ∀ a. J.DecodeJson a ⇒ String → Either String (Maybe a)
-    lookupJSON = traverse fromJSONString <<< lookup
+  Tuple hdfsHost props' ← case SM.pop "hdfsUrl" props of
+    Just (Tuple (Just value) rest) → do
+      value' ← extractHost' hdfsURIScheme $ decodeURIComponent value
+      pure (Tuple value' rest)
+    _ → Left "Expected `hdfsUrl` query parameter"
 
-  hdfsHost ← lookup "hdfsUrl" # maybe
-    (Left "Expected `hdfsUrl` query parameter")
-    (extractHost' hdfsURIScheme <<< decodeURIComponent)
+  Tuple path props'' ← case SM.pop "rootPath" props' of
+    Just (Tuple (Just value) rest) → do
+      value' ← bimap show Just $ runParser parseURIPathAbs value
+      pure (Tuple value' rest)
+    _ → Left "Expected `rootPath` query parameter"
 
-  path ← lookup "rootPath" # maybe
-    (Left "Expected `rootPath` query parameter")
-    (bimap show Just <<< runParser parseURIPathAbs)
+  pure { sparkHost, hdfsHost, path, props: props'' }
 
-  let
-    executorMemory = lookup "spark.executor.memory"
-    executorExtraJavaOptions = lookup "spark.executor.extraJavaOptions"
-    driverMaxResultSize = lookup "spark.driver.maxResultSize"
-    driverMemory = lookup "spark.driver.memory"
-    localDir = lookup "spark.local.dir"
-    reducerMaxSizeInFlight = lookup "spark.reducer.maxSizeInFlight"
-    shuffleFileBuffer = lookup "spark.shuffle.file.buffer"
-    shuffleIoRetryWait = lookup "spark.shuffle.io.retryWait"
-
-  executorCores ← lookupJSON "spark.executor.cores"
-  defaultParallelism ← lookupJSON "spark.default.parallelism"
-  filesMaxPartitionBytes ← lookupJSON "spark.files.maxPartitionBytes"
-  driverCores ← lookupJSON "spark.driver.cores"
-  reducerMaxReqsInFlight ← lookupJSON "spark.reducer.maxReqsInFlight"
-  memoryFraction ← lookupJSON "spark.memory.fraction"
-  memoryStorageFraction ← lookupJSON "spark.memory.storageFraction"
-  coresMax ← lookupJSON "spark.cores.max"
-  speculation ← lookupJSON "spark.speculation"
-  tasksCpus ← lookupJSON "spark.tasks.cpus"
-
-  pure
-    { sparkHost
-    , hdfsHost
-    , path
-    , executorMemory
-    , executorCores
-    , executorExtraJavaOptions
-    , defaultParallelism
-    , filesMaxPartitionBytes
-    , driverCores
-    , driverMaxResultSize
-    , driverMemory
-    , localDir
-    , reducerMaxSizeInFlight
-    , reducerMaxReqsInFlight
-    , shuffleFileBuffer
-    , shuffleIoRetryWait
-    , memoryFraction
-    , memoryStorageFraction
-    , coresMax
-    , speculation
-    , tasksCpus
-    }
+mkURI ∷ URI.URIScheme → Host → Maybe URI.Query → URI.AbsoluteURI
+mkURI scheme host params =
+  URI.AbsoluteURI
+    (Just scheme)
+    (URI.HierarchicalPart (Just (URI.Authority Nothing (pure host))) Nothing)
+    params
 
 extractHost' ∷ URI.URIScheme → String → Either String Host
 extractHost' scheme@(URI.URIScheme name) uri = do
