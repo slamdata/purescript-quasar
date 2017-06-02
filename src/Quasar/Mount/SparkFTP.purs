d0725f7e99bf85e8dff00aae2e80e3cd9259d644
Cmdv <vincent@cmdv.me>
2017-06-02 19:36:32 +0100

    Broken types on toURI

--- src/Quasar/Mount/SparkFTP.purs
+++ src/Quasar/Mount/SparkFTP.purs
@@ -28,10 +28,15 @@ import Prelude
 import Data.Argonaut (Json, decodeJson, jsonEmptyObject, (.?), (:=), (~>))
 import Data.Bifunctor (bimap, lmap)
 import Data.Either (Either(..))
+import Data.List as L
 import Data.Maybe (Maybe(..), maybe)
 import Data.String (Pattern(..), joinWith, split)
+import Data.StrMap as SM
+import Data.Tuple (Tuple(..))
 import Data.URI as URI
 import Data.URI.Path (printPath, parseURIPathAbs)
+
+import Global (encodeURIComponent)
 
 import Quasar.Mount.Common (Host, extractHost)
 import Quasar.Mount.Common (Host) as Exports
@@ -39,16 +44,21 @@ import Quasar.Types (AnyPath)
 
 import Text.Parsing.StringParser (runParser)
 
+
 type Config =
   { sparkHost ∷ Host
   , ftpHost ∷ Host
   , path ∷ Maybe AnyPath
+  , user ∷ Maybe String
+  , password ∷ Maybe String
+  , props ∷ String
   }
 
 toJSON ∷ Config → Json
 toJSON config =
-  let uri = toString config
-  in "spark-ftp" := ("connectionUri" := uri ~> jsonEmptyObject) ~> jsonEmptyObject
+  let uri = URI.printAbsoluteURI (toURI config)
+  in "spark-hdfs" := ("connectionUri" := uri ~> jsonEmptyObject) ~> jsonEmptyObject
+
 
 fromJSON ∷ Json → Either String Config
 fromJSON
@@ -56,6 +66,24 @@ fromJSON
   <=< (_ .? "connectionUri")
   <=< (_ .? "spark")
   <=< decodeJson
+
+toURI ∷ Config → URI.AbsoluteURI
+toURI cfg = mkURI sparkURIScheme cfg.sparkHost (Just (URI.Query $ requiredProps <> optionalProps))
+  where
+    requiredProps ∷ L.List (Tuple String (Maybe String))
+    requiredProps = L.fromFoldable
+      [ Tuple "hdfsUrl" $ Just $ encodeURIComponent $ URI.printAbsoluteURI $ mkURI ftpURIScheme cfg.ftpHost Nothing
+      , Tuple "rootPath" $ Just $ maybe "/" printPath cfg.path
+      ]
+    optionalProps ∷ L.List (Tuple String (Maybe String))
+    optionalProps = SM.toUnfoldable cfg.props
+    mkURI ∷ URI.URIScheme → Host → URI.AbsoluteURI
+    mkURI scheme host =
+      URI.AbsoluteURI
+        (Just scheme)
+        (URI.HierarchicalPart (Just (URI.Authority Nothing (pure host))) Nothing)
+        Nothing
+
 
 toString ∷ Config → String
 toString { sparkHost, ftpHost, path } =
