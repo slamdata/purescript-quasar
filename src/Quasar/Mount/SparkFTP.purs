3edc9921be7ed5c4bd7077ee608964a30f38f007
Cmdv <vincent@cmdv.me>
2017-06-04 00:24:01 +0100

    adjust SparkFTP Mount

--- src/Quasar/Mount/SparkFTP.purs
+++ src/Quasar/Mount/SparkFTP.purs
@@ -18,25 +18,25 @@ module Quasar.Mount.SparkFTP
   ( Config
   , toJSON
   , fromJSON
-  , toString
-  , fromString
+  , toURI
+  , fromURI
   , module Exports
   ) where
 
 import Prelude
 
-import Data.Argonaut (Json, decodeJson, jsonEmptyObject, (.?), (:=), (~>))
+import Data.Argonaut (Json, (.?), (:=), (~>))
+import Data.Argonaut as J
 import Data.Bifunctor (bimap, lmap)
 import Data.Either (Either(..))
 import Data.List as L
 import Data.Maybe (Maybe(..), maybe)
-import Data.String (Pattern(..), joinWith, split)
 import Data.StrMap as SM
 import Data.Tuple (Tuple(..))
 import Data.URI as URI
 import Data.URI.Path (printPath, parseURIPathAbs)
 
-import Global (encodeURIComponent)
+import Global (encodeURIComponent, decodeURIComponent)
 
 import Quasar.Mount.Common (Host, extractHost)
 import Quasar.Mount.Common (Host) as Exports
@@ -44,85 +44,77 @@ import Quasar.Types (AnyPath)
 
 import Text.Parsing.StringParser (runParser)
 
-
 type Config =
   { sparkHost ∷ Host
   , ftpHost ∷ Host
   , path ∷ Maybe AnyPath
-  , user ∷ Maybe String
+  , props ∷ SM.StrMap (Maybe String)
   , password ∷ Maybe String
-  , props ∷ String
+  , username ∷ Maybe String
   }
 
 toJSON ∷ Config → Json
 toJSON config =
   let uri = URI.printAbsoluteURI (toURI config)
-  in "spark-hdfs" := ("connectionUri" := uri ~> jsonEmptyObject) ~> jsonEmptyObject
-
+  in "spark-hdfs" := ("connectionUri" := uri ~> J.jsonEmptyObject) ~> J.jsonEmptyObject
 
 fromJSON ∷ Json → Either String Config
 fromJSON
-  = fromString
+  = fromURI
+  <=< lmap show <<< URI.runParseAbsoluteURI
   <=< (_ .? "connectionUri")
-  <=< (_ .? "spark")
-  <=< decodeJson
+  <=< (_ .? "spark-hdfs")
+  <=< J.decodeJson
 
 toURI ∷ Config → URI.AbsoluteURI
 toURI cfg = mkURI sparkURIScheme cfg.sparkHost (Just (URI.Query $ requiredProps <> optionalProps))
   where
-    requiredProps ∷ L.List (Tuple String (Maybe String))
-    requiredProps = L.fromFoldable
-      [ Tuple "hdfsUrl" $ Just $ encodeURIComponent $ URI.printAbsoluteURI $ mkURI ftpURIScheme cfg.ftpHost Nothing
-      , Tuple "rootPath" $ Just $ maybe "/" printPath cfg.path
-      ]
-    optionalProps ∷ L.List (Tuple String (Maybe String))
-    optionalProps = SM.toUnfoldable cfg.props
-    mkURI ∷ URI.URIScheme → Host → URI.AbsoluteURI
-    mkURI scheme host =
-      URI.AbsoluteURI
-        (Just scheme)
-        (URI.HierarchicalPart (Just (URI.Authority Nothing (pure host))) Nothing)
-        Nothing
-
-
-toString ∷ Config → String
-toString { sparkHost, ftpHost, path } =
-  joinWith "|"
-    [ URI.printAbsoluteURI (mkURI sparkURIScheme sparkHost)
-    , URI.printAbsoluteURI (mkURI ftpURIScheme ftpHost)
-    , maybe "/" printPath path
+  requiredProps ∷ L.List (Tuple String (Maybe String))
+  requiredProps = L.fromFoldable
+    [ Tuple "hdfsUrl" $ Just $ encodeURIComponent $ URI.printAbsoluteURI $ mkURI hdfsURIScheme cfg.ftpHost Nothing
+    , Tuple "rootPath" $ Just $ maybe "/" printPath cfg.path
     ]
-  where
-  mkURI ∷ URI.URIScheme → Host → URI.AbsoluteURI
-  mkURI scheme host =
-    URI.AbsoluteURI
-      (Just scheme)
-      (URI.HierarchicalPart (Just (URI.Authority Nothing (pure host))) Nothing)
-      Nothing
 
-fromString ∷ String → Either String Config
-fromString str = case split (Pattern "|") str of
-  [ spark, ftp, root ] → do
-    sparkHost ← extractHost' sparkURIScheme spark
-    ftpHost ← extractHost' ftpURIScheme ftp
-    path ← bimap show Just (runParser parseURIPathAbs root)
-    pure
-      { sparkHost
-      , ftpHost
-      , path
-      }
-  _ →
-    Left "Expected 'spark' connectionUri format"
-  where
-  extractHost' ∷ URI.URIScheme → String → Either String Host
-  extractHost' scheme@(URI.URIScheme name) uri = do
-    URI.AbsoluteURI scheme' (URI.HierarchicalPart auth _) _ ←
-      lmap show $ URI.runParseAbsoluteURI uri
-    unless (scheme' == Just scheme) $ Left $ "Expected '" <> name <> "' URL scheme"
-    extractHost auth
+  optionalProps ∷ L.List (Tuple String (Maybe String))
+  optionalProps = SM.toUnfoldable cfg.props
+
+fromURI ∷ URI.AbsoluteURI → Either String Config
+fromURI (URI.AbsoluteURI scheme (URI.HierarchicalPart auth _) query) = do
+  unless (scheme == Just sparkURIScheme) $ Left "Expected `spark` URL scheme"
+  sparkHost ← extractHost auth
+  let props = maybe SM.empty (\(URI.Query qs) → SM.fromFoldable qs) query
+
+  Tuple ftpHost props' ← case SM.pop "hdfsUrl" props of
+    Just (Tuple (Just value) rest) → do
+      value' ← extractHost' hdfsURIScheme $ decodeURIComponent value
+      pure (Tuple value' rest)
+    _ → Left "Expected `hdfsUrl` query parameter"
+
+  Tuple path props'' ← case SM.pop "rootPath" props' of
+    Just (Tuple (Just value) rest) → do
+      -- I think that aorund here I need to handle the username/password eg ftp://login:password@host:port
+      value' ← bimap show Just $ runParser parseURIPathAbs value
+      pure (Tuple value' rest)
+    _ → Left "Expected `rootPath` query parameter"
+
+  pure { sparkHost, ftpHost, path, props: props'' }
+
+mkURI ∷ URI.URIScheme → Host → Maybe URI.Query → URI.AbsoluteURI
+mkURI scheme host params =
+  URI.AbsoluteURI
+    (Just scheme)
+    (URI.HierarchicalPart (Just (URI.Authority Nothing (pure host))) Nothing)
+    params
+
+extractHost' ∷ URI.URIScheme → String → Either String Host
+extractHost' scheme@(URI.URIScheme name) uri = do
+  URI.AbsoluteURI scheme' (URI.HierarchicalPart auth _) _ ←
+    lmap show $ URI.runParseAbsoluteURI uri
+  unless (scheme' == Just scheme) $ Left $ "Expected '" <> name <> "' URL scheme"
+  extractHost auth
 
 sparkURIScheme ∷ URI.URIScheme
 sparkURIScheme = URI.URIScheme "spark"
 
-ftpURIScheme ∷ URI.URIScheme
-ftpURIScheme = URI.URIScheme "ftp"
+hdfsURIScheme ∷ URI.URIScheme
+hdfsURIScheme = URI.URIScheme "hdfs"
