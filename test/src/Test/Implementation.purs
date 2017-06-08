42f77a6b4a2b6e30c6cd0c59375356545eaebb0a
Gary Burgess <gary.burgess@gmail.com>
2017-06-08 22:46:10 +0100

    Rename Test.Fast to Test.Implementation

--- test/src/Test/Implementation.purs
+++ test/src/Test/Implementation.purs
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -}
 
-module Test.Fast where
+module Test.Implementation where
 
 import Prelude
 
