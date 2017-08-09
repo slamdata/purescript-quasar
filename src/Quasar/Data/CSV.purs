a4c25bc484ccc8f5a65c1570939fdcb6d89379d7
Cmdv <vincent@cmdv.me>
2017-08-09 17:28:03 +0100

    add show to handle escaping

--- src/Quasar/Data/CSV.purs
+++ src/Quasar/Data/CSV.purs
@@ -40,8 +40,8 @@ derive instance genericOptions ∷ Generic Options _
 defaultOptions ∷ Options
 defaultOptions =
   Options
-    { columnDelimiter: "\",\""
-    , rowDelimiter: "\"\\n\""
+    { columnDelimiter: ","
+    , rowDelimiter: "\\n"
     , quoteChar: "\""
     , escapeChar: "\""
     }
@@ -51,8 +51,8 @@ toMediaType (Options opts) =
   MediaType $
     String.joinWith ";"
       [ "text/csv"
-      , "columnDelimiter=" <> opts.columnDelimiter
-      , "rowDelimiter=" <> opts.rowDelimiter
-      , "quoteChar=" <> opts.quoteChar
-      , "escapeChar=" <> opts.escapeChar
+      , "columnDelimiter=" <> show opts.columnDelimiter
+      , "rowDelimiter=" <> show opts.rowDelimiter
+      , "quoteChar=" <> show opts.quoteChar
+      , "escapeChar=" <> show opts.escapeChar
       ]
