f35625d7bc269d3373a8f8910234868f9c449f21
Nathan Faubion <nathan@n-son.com>
2017-08-15 08:21:50 -0700

    Add TypeStats codecs

--- src/Quasar/Schema/TypeStats.purs
+++ src/Quasar/Schema/TypeStats.purs
@@ -17,55 +17,71 @@ limitations under the License.
 module Quasar.Schema.TypeStats where
 
 import Prelude
+
+import Control.Monad.Error.Class (throwError)
+import Data.Argonaut as J
+import Data.Array as Array
+import Data.Bifunctor (lmap)
+import Data.Codec as C
+import Data.Codec.Argonaut as CA
+import Data.Either (Either)
+import Data.Foldable (foldMap)
 import Data.Generic (class Generic, gShow)
 import Data.HugeInt (HugeInt)
+import Data.HugeInt as HI
 import Data.HugeNum (HugeNum)
-import Data.Maybe (Maybe)
+import Data.HugeNum as HN
+import Data.List as List
+import Data.Maybe (Maybe, maybe)
+import Data.StrMap as StrMap
+import Data.String as Str
+import Data.Symbol (SProxy(..))
+import Data.Tuple (Tuple(..))
 
 type BoolStats =
-  { true :: Number
-  , false :: Number
+  { true ∷ Number
+  , false ∷ Number
   }
 
 type CharStats =
-  { count :: Number
-  , min :: Char
-  , max :: Char
+  { count ∷ Number
+  , min ∷ Char
+  , max ∷ Char
   }
 
 type StringStats =
-  { count :: Number
-  , minLength :: Number
-  , maxLength :: Number
-  , min :: String
-  , max :: String
+  { count ∷ Number
+  , minLength ∷ Number
+  , maxLength ∷ Number
+  , min ∷ String
+  , max ∷ String
   }
 
 type IntStats =
-  { count :: Number
-  , distribution :: Distribution
-  , min :: HugeInt
-  , max :: HugeInt
+  { count ∷ Number
+  , distribution ∷ Distribution
+  , min ∷ HugeInt
+  , max ∷ HugeInt
   }
 
 type DecimalStats =
-  { count :: Number
-  , distribution :: Distribution
-  , min :: HugeNum
-  , max :: HugeNum
+  { count ∷ Number
+  , distribution ∷ Distribution
+  , min ∷ HugeNum
+  , max ∷ HugeNum
   }
 
 type CollectionStats =
-  { count :: Number
-  , minLength :: Maybe Number
-  , maxLength :: Maybe Number
+  { count ∷ Number
+  , minLength ∷ Maybe Number
+  , maxLength ∷ Maybe Number
   }
 
 type Distribution =
-  { mean :: Number
-  , variance :: Number
-  , skewness :: Number
-  , kurtosis :: Number
+  { mean ∷ Number
+  , variance ∷ Number
+  , skewness ∷ Number
+  , kurtosis ∷ Number
   }
 
 data TypeStats
@@ -85,3 +101,122 @@ derive instance genericTypeStats ∷ Generic TypeStats
 
 instance showTypeStats ∷ Show TypeStats where
   show = gShow
+
+encode ∷ TypeStats → J.Json
+encode = CA.encode codec
+
+decode ∷ J.Json → Either String TypeStats
+decode = lmap CA.printJsonDecodeError <<< CA.decode codec
+
+codec ∷ CA.JsonCodec TypeStats
+codec = C.basicCodec dec enc
+  where
+  dec json = do
+    obj ← C.decode CA.jobject json
+    tag ← C.decode (CA.prop "kind" CA.string) obj
+    case tag of
+      "bool" → Bool <$> C.decode boolCodec obj
+      "char" → Char <$> C.decode charCodec obj
+      "string" → String <$> C.decode stringCodec obj
+      "integer" → Int <$> C.decode intCodec obj
+      "decimal" → Decimal <$> C.decode decimalCodec obj
+      "collection" → Collection <$> C.decode collectionCodec obj
+      "count" → Count <<< _.count <$> C.decode countCodec obj
+      _ → throwError (CA.UnexpectedValue json)
+
+  enc = case _ of
+    Bool st → kinded "bool" (C.encode boolCodec st)
+    Char st → kinded "char" (C.encode charCodec st)
+    String st → kinded "string" (C.encode stringCodec st)
+    Int st → kinded "integer" (C.encode intCodec st)
+    Decimal st → kinded "decimal" (C.encode decimalCodec st)
+    Collection st → kinded "collection" (C.encode collectionCodec st)
+    Count count → kinded "count" (C.encode countCodec { count })
+
+  kinded str =
+    List.Cons (Tuple "kind" (J.fromString str))
+      >>> StrMap.fromFoldable
+      >>> J.fromObject
+
+boolCodec ∷ CA.JPropCodec BoolStats
+boolCodec =
+  CA.record
+    # CA.recordProp (SProxy ∷ SProxy "true") CA.number
+    # CA.recordProp (SProxy ∷ SProxy "false") CA.number
+
+charCodec ∷ CA.JPropCodec CharStats
+charCodec =
+  CA.record
+    # CA.recordProp (SProxy ∷ SProxy "count") CA.number
+    # CA.recordProp (SProxy ∷ SProxy "min") CA.char
+    # CA.recordProp (SProxy ∷ SProxy "max") CA.char
+
+stringCodec ∷ CA.JPropCodec StringStats
+stringCodec =
+  CA.record
+    # CA.recordProp (SProxy ∷ SProxy "count") CA.number
+    # CA.recordProp (SProxy ∷ SProxy "minLength") CA.number
+    # CA.recordProp (SProxy ∷ SProxy "maxLength") CA.number
+    # CA.recordProp (SProxy ∷ SProxy "min") CA.string
+    # CA.recordProp (SProxy ∷ SProxy "max") CA.string
+
+intCodec ∷ CA.JPropCodec IntStats
+intCodec =
+  CA.record
+    # CA.recordProp (SProxy ∷ SProxy "count") CA.number
+    # CA.recordProp (SProxy ∷ SProxy "distribution") distributionCodec
+    # CA.recordProp (SProxy ∷ SProxy "min") hugeIntCodec
+    # CA.recordProp (SProxy ∷ SProxy "max") hugeIntCodec
+
+decimalCodec ∷ CA.JPropCodec DecimalStats
+decimalCodec =
+  CA.record
+    # CA.recordProp (SProxy ∷ SProxy "count") CA.number
+    # CA.recordProp (SProxy ∷ SProxy "distribution") distributionCodec
+    # CA.recordProp (SProxy ∷ SProxy "min") hugeNumCodec
+    # CA.recordProp (SProxy ∷ SProxy "max") hugeNumCodec
+
+collectionCodec ∷ CA.JPropCodec CollectionStats
+collectionCodec =
+  CA.record
+    # CA.recordProp (SProxy ∷ SProxy "count") CA.number
+    # CA.recordProp (SProxy ∷ SProxy "minLength") (optionalCodec CA.number)
+    # CA.recordProp (SProxy ∷ SProxy "maxLength") (optionalCodec CA.number)
+
+countCodec ∷ CA.JPropCodec { count ∷ Number }
+countCodec =
+  CA.record
+    # CA.recordProp (SProxy ∷ SProxy "count") CA.number
+
+distributionCodec ∷ CA.JsonCodec Distribution
+distributionCodec =
+  CA.object "Distribution" $ CA.record
+    # CA.recordProp (SProxy ∷ SProxy "mean") CA.number
+    # CA.recordProp (SProxy ∷ SProxy "variance") CA.number
+    # CA.recordProp (SProxy ∷ SProxy "skewness") CA.number
+    # CA.recordProp (SProxy ∷ SProxy "kurtosis") CA.number
+
+hugeIntCodec ∷ CA.JsonCodec HugeInt
+hugeIntCodec =
+  C.basicCodec
+    (C.decode CA.string
+      >=> HI.fromString
+      >>> maybe (throwError (CA.TypeMismatch "HugeInt")) pure)
+    (HI.toHugeNum
+      >>> HN.toString
+      >>> Str.takeWhile (not eq '.')
+      >>> J.fromString)
+
+hugeNumCodec ∷ CA.JsonCodec HugeNum
+hugeNumCodec =
+  C.basicCodec
+    (C.decode CA.string
+      >=> HN.fromString
+      >>> maybe (throwError (CA.TypeMismatch "HugeNum")) pure)
+    (HN.toString >>> J.fromString)
+
+optionalCodec ∷ ∀ a. CA.JsonCodec a → CA.JsonCodec (Maybe a)
+optionalCodec codec =
+  C.basicCodec
+    (map Array.head <$> C.decode (CA.array codec))
+    (foldMap pure >>> CA.encode (CA.array codec))
